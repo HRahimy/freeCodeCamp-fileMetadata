e1f73bc931344b044873e121b1411ccd34f54c55
Hamza Rahimy <rahimy.hamza@gmail.com>
2022-07-28 08:51:33 +0000

    Implement file upload endpoint under `POST '/api/fileanalyse'`

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require("body-parser");
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 require('dotenv').config()
-
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +20,16 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+// Reference to answer for using `upload.single('upfile')` correctly:
+// https://stackoverflow.com/a/31556381/5472560
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const returnDto = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+  res.json(returnDto);
+});
 
 
 const port = process.env.PORT || 3000;
